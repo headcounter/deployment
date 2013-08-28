c77973b35bc5f4fc896335db3b8c5ac0e7d90949
aszlig <aszlig@redmoonstudios.org>
2013-08-28 02:50:16 +0200

    Initial deployment of ultron and benteflork.
    This is just a basic network.nix with the partitioning information and
    some nix options for benteflork, nothing special yet.
    
    Signed-off-by: aszlig <aszlig@redmoonstudios.org>

---
+++ network.nix
@@ -0,0 +1,44 @@
+{
+  network.description = "Headcounter Services";
+  network.enableRollback = true;
+
+  ultron = {
+    deployment.targetEnv = "hetzner";
+    deployment.hetzner.mainIPv4 = "5.9.105.142";
+    deployment.hetzner.partitions = ''
+      clearpart --all --initlabel --drives=sda,sdb
+
+      part swap1 --recommended --label=swap1 --fstype=swap --ondisk=sda
+      part swap2 --recommended --label=swap2 --fstype=swap --ondisk=sdb
+
+      part btrfs.1 --grow --ondisk=sda
+      part btrfs.2 --grow --ondisk=sdb
+
+      btrfs / --data=1 --metadata=1 --label=root btrfs.1 btrfs.2
+    '';
+  };
+
+  benteflork = { pkgs, config, ... }: {
+    deployment.targetEnv = "hetzner";
+    deployment.hetzner.mainIPv4 = "144.76.61.117";
+    deployment.hetzner.partitions = ''
+      clearpart --all --initlabel --drives=sda,sdb
+
+      part swap1 --size=20000 --label=swap1 --fstype=swap --ondisk=sda
+      part swap2 --size=20000 --label=swap2 --fstype=swap --ondisk=sdb
+
+      part raid.1 --grow --ondisk=sda
+      part raid.2 --grow --ondisk=sdb
+
+      raid / --level=1 --device=md0 --fstype=ext4 --label=root raid.1 raid.2
+    '';
+
+    nix = {
+      useChroot = true;
+      readOnlyStore = true;
+      extraOptions = ''
+        build-cores = 0
+      '';
+    };
+  };
+}
