8fc1ffe026d3d516e576d253ecc155b5224c26bb
aszlig <aszlig@redmoonstudios.org>
2013-10-30 02:52:24 +0100

    domains: Add SSL (intermediate) certificates.
    Of course we only provide the public keys here. Private keys will of
    course be kept private ;-)
    
    So, having this in Git, other people can possibly cross-check the
    validity of the public keys in addition to just checking whether it has
    been signed by an established CA.
    
    Signed-off-by: aszlig <aszlig@redmoonstudios.org>

--- domains.nix
+++ domains.nix
@@ -1,32 +1,41 @@
-{
+let
+  privateKeys = import ./ssl/private.nix;
+  publicKeys = import ./ssl/public.nix;
+
+  withSSL = attrs: attrs // {
+    ssl.privateKey = builtins.getAttr attrs.fqdn privateKeys;
+    ssl.publicKey = builtins.getAttr attrs.fqdn publicKeys;
+    ssl.intermediateCert = builtins.readFile ./ssl/intermediate.crt;
+  };
+in {
   imports = [ ./modules/vhosts.nix ];
 
   vhosts = {
-    headcounter = {
+    headcounter = withSSL {
       fqdn = "headcounter.org";
       ipv4 = "78.47.32.128";
       ipv6 = "2a01:4f8:162:4187::1";
     };
 
-    jabber = {
+    jabber = withSSL {
       fqdn = "jabber.headcounter.org";
       ipv4 = "78.47.32.129";
       ipv6 = "2a01:4f8:162:4187::2";
     };
 
-    aszlig = {
+    aszlig = withSSL {
       fqdn = "aszlig.net";
       ipv4 = "78.47.32.130";
       ipv6 = "2a01:4f8:162:4187::3";
     };
 
-    noicq = {
+    noicq = withSSL {
       fqdn = "noicq.org";
       ipv4 = "78.47.32.131";
       ipv6 = "2a01:4f8:162:4187::4";
     };
 
-    no_icq = {
+    no_icq = withSSL {
       fqdn = "noicq.org";
       ipv4 = "78.47.32.132";
       ipv6 = "2a01:4f8:162:4187::5";
