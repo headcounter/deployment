b0242c151e708f6cfd21779341fdfd2bc67bef92
aszlig <aszlig@redmoonstudios.org>
2013-10-31 08:00:11 +0100

    ultron: Provide vhost config for IPv6 as well.
    This solves the problem of using use-ipv6, because we now bind to the
    respective address directly rather than on all interfaces.
    
    In addition, we now generate SSL vhosts based on the VHost modular
    configuration, so we have less duplication over there.
    
    Signed-off-by: aszlig <aszlig@redmoonstudios.org>

--- machines/ultron.nix
+++ machines/ultron.nix
@@ -2,7 +2,23 @@
 
 with pkgs.lib;
 
-{
+let
+  genSSLVHost = vhost: configuration: let
+    genConf = sock: {
+      type = "static";
+      on = vhost.fqdn;
+      socket = "${sock}:443";
+
+      socketConfig = ''
+        ssl.engine      = "enable"
+        ssl.pemfile     = "${vhost.ssl.privateKey.path}"
+        ssl.ca-file     = "${vhost.ssl.intermediateCert}"
+      '';
+
+      inherit configuration;
+    };
+  in [ (genConf vhost.ipv4) (genConf "[${vhost.ipv6}]") ];
+in {
   imports = [ ../hydra.nix ../lighttpd.nix ../domains.nix ];
 
   services.headcounter.lighttpd = {
@@ -18,35 +34,22 @@ with pkgs.lib;
     modules.magnet.enable = true;
     modules.setenv.enable = true;
 
-    virtualHosts = singleton {
-      type = "static";
-      on = "headcounter.org";
-      socket = ":443";
-
-      socketConfig = with config.vhosts.headcounter.ssl; ''
-        server.use-ipv6 = "enable"
-        ssl.engine      = "enable"
-        ssl.pemfile     = "${privateKey.path}"
-        ssl.ca-file     = "${intermediateCert}"
-      '';
-
-      configuration = ''
-        $HTTP["url"] =~ "^/hydra(?:$|/)" {
-          magnet.attract-physical-path-to = ( "${pkgs.writeText "rewrite.lua" ''
-          if string.sub(lighty.env["request.uri"], 1, 6) == "/hydra" then
-            lighty.env["request.uri"] = string.sub(lighty.env["request.uri"], 7)
-          end
-          ''}" )
-          setenv.add-request-header = (
-            "X-Request-Base" => "https://headcounter.org/hydra/"
-          )
-          proxy.balance = "hash"
-          proxy.server = ("/hydra" => ((
-            "host" => "127.0.0.1",
-            "port" => 3000
-          )))
-        }
-      '';
-    };
+    virtualHosts = with config.vhosts; genSSLVHost headcounter ''
+      $HTTP["url"] =~ "^/hydra(?:$|/)" {
+        magnet.attract-physical-path-to = ( "${pkgs.writeText "rewrite.lua" ''
+        if string.sub(lighty.env["request.uri"], 1, 6) == "/hydra" then
+          lighty.env["request.uri"] = string.sub(lighty.env["request.uri"], 7)
+        end
+        ''}" )
+        setenv.add-request-header = (
+          "X-Request-Base" => "https://headcounter.org/hydra/"
+        )
+        proxy.balance = "hash"
+        proxy.server = ("/hydra" => ((
+          "host" => "127.0.0.1",
+          "port" => 3000
+        )))
+      }
+    '';
   };
 }
