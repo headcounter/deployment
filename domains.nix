243c4f81a0f366f5451eded5788ec9d3f197e119
aszlig <aszlig@redmoonstudios.org>
2013-10-31 07:47:57 +0100

    domains: Fix off-by-one error in IPv4 addresses.
    Signed-off-by: aszlig <aszlig@redmoonstudios.org>

--- domains.nix
+++ domains.nix
@@ -13,31 +13,31 @@ in {
   vhosts = {
     headcounter = withSSL {
       fqdn = "headcounter.org";
-      ipv4 = "78.47.32.128";
+      ipv4 = "78.47.32.129";
       ipv6 = "2a01:4f8:162:4187::1";
     };
 
     jabber = withSSL {
       fqdn = "jabber.headcounter.org";
-      ipv4 = "78.47.32.129";
+      ipv4 = "78.47.32.130";
       ipv6 = "2a01:4f8:162:4187::2";
     };
 
     aszlig = withSSL {
       fqdn = "aszlig.net";
-      ipv4 = "78.47.32.130";
+      ipv4 = "78.47.32.131";
       ipv6 = "2a01:4f8:162:4187::3";
     };
 
     noicq = withSSL {
       fqdn = "noicq.org";
-      ipv4 = "78.47.32.131";
+      ipv4 = "78.47.32.132";
       ipv6 = "2a01:4f8:162:4187::4";
     };
 
     no_icq = withSSL {
       fqdn = "noicq.org";
-      ipv4 = "78.47.32.132";
+      ipv4 = "78.47.32.133";
       ipv6 = "2a01:4f8:162:4187::5";
     };
   };
