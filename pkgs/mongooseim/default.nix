fa1ee8da2a960694303f92d1cea44e74788801cd
aszlig <aszlig@redmoonstudios.org>
2013-11-04 04:02:19 +0100

    pkgs/mongooseim: Add meta attributes.
    Signed-off-by: aszlig <aszlig@redmoonstudios.org>

--- pkgs/mongooseim/default.nix
+++ pkgs/mongooseim/default.nix
@@ -1,4 +1,4 @@
-{ buildErlang, fetchurl, pam, zlib, openssl, expat
+{ stdenv, buildErlang, fetchurl, pam, zlib, openssl, expat
 , cuesport, redo, exml, lager, cowboy, folsom, mochijson2, alarms
 }:
 
@@ -36,4 +36,11 @@ buildErlang rec {
   postInstall = ''
     cp -a rel/ejabberd "$out"
   '';
+
+  meta = {
+    homepage = "https://www.erlang-solutions.com/products/"
+             + "mongooseim-massively-scalable-ejabberd-platform";
+    description = "An Ejabberd fork utilizing Erlang/OTP features.";
+    license = stdenv.lib.licenses.gpl2Plus;
+  };
 }
