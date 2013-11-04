38735ceed60da08810ec5cb93b94c7ff61b43161
aszlig <aszlig@redmoonstudios.org>
2013-11-04 04:00:34 +0100

    network: Move ultron's partitioning into machines.
    This is machine-specific, so let's not clutter up network.nix too much
    with that.
    
    Signed-off-by: aszlig <aszlig@redmoonstudios.org>

--- machines/ultron.nix
+++ machines/ultron.nix
@@ -21,6 +21,18 @@ let
 in {
   imports = [ ../hydra.nix ../modules/services/lighttpd.nix ../domains.nix ];
 
+  deployment.hetzner.partitions = ''
+    clearpart --all --initlabel --drives=sda,sdb
+
+    part swap1 --recommended --label=swap1 --fstype=swap --ondisk=sda
+    part swap2 --recommended --label=swap2 --fstype=swap --ondisk=sdb
+
+    part btrfs.1 --grow --ondisk=sda
+    part btrfs.2 --grow --ondisk=sdb
+
+    btrfs / --data=1 --metadata=1 --label=root btrfs.1 btrfs.2
+  '';
+
   boot.kernelPackages = pkgs.linuxPackages_3_10;
 
   fileSystems."/".options = concatStringsSep "," [
--- network.nix
+++ network.nix
@@ -5,20 +5,8 @@
   resources.sshKeyPairs."hydra-build" = {};
 
   ultron = { pkgs, config, ... }: {
-    deployment.hetzner.mainIPv4 = "5.9.105.142";
-    deployment.hetzner.partitions = ''
-      clearpart --all --initlabel --drives=sda,sdb
-
-      part swap1 --recommended --label=swap1 --fstype=swap --ondisk=sda
-      part swap2 --recommended --label=swap2 --fstype=swap --ondisk=sdb
-
-      part btrfs.1 --grow --ondisk=sda
-      part btrfs.2 --grow --ondisk=sdb
-
-      btrfs / --data=1 --metadata=1 --label=root btrfs.1 btrfs.2
-    '';
-
     imports = [ ./common.nix ./machines/ultron.nix ];
+    deployment.hetzner.mainIPv4 = "5.9.105.142";
   };
 
   taalo = { pkgs, config, ... }: {
