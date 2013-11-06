935c1d1374986e22baf03312f6bd86a9c86bfa2b
aszlig <aszlig@redmoonstudios.org>
2013-11-06 10:12:56 +0100

    release.nix: Properly map through .test attrs.
    Also, let's explicitely set the system architecture.
    
    Signed-off-by: aszlig <aszlig@redmoonstudios.org>

--- release.nix
+++ release.nix
@@ -1,3 +1,17 @@
-{
-  tests = import ./tests;
+{ nixpkgs ? <nixpkgs>
+}:
+
+let
+  pkgs = import nixpkgs { system = "x86_64-linux"; };
+  systems = [ "x86_64-linux" ];
+in {
+  tests = with pkgs.lib; let
+    testsOnly = attrs: !attrs ? test;
+    testsFor = system: let
+      mapper = mapAttrsRecursiveCond testsOnly sysAttrs;
+      sysAttrs = _: val: listToAttrs [(nameValuePair system val.test)];
+    in mapper (import ./tests {
+      inherit nixpkgs system;
+    });
+  in fold recursiveUpdate {} (map testsFor systems);
 }
