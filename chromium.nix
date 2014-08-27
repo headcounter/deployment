d50ce9b2dc086d402b2b0ba7afec98b68aadb8c5
aszlig <aszlig@redmoonstudios.org>
2014-08-27 17:52:59 +0200

    chromium: Autoupdate with full nixpkgs tree.
    The refactored Chromium update script is now relying on <nixpkgs> for
    some functionality, so let's simply update the full nixpkgs tree here,
    even though we only build the Chromium channels.
    
    Signed-off-by: aszlig <aszlig@redmoonstudios.org>

--- chromium.nix
+++ chromium.nix
@@ -31,15 +31,12 @@ in {
       if [ ! -e .git ]; then
         chmod go+rx "${home}"
         ${pkgs.git}/bin/git init
-        ${pkgs.git}/bin/git config core.sparsecheckout true
         ${pkgs.git}/bin/git config http.sslVerify false
 
         ${pkgs.git}/bin/git config user.name 'Chromium Autoupdater'
         ${pkgs.git}/bin/git config user.email \
           'chromium-autoupdate@headcounter.org'
 
-        echo pkgs/applications/networking/browsers/chromium \
-          > .git/info/sparse-checkout
         ${pkgs.git}/bin/git remote add origin \
           https://github.com/NixOS/nixpkgs.git
       fi
@@ -49,7 +46,7 @@ in {
         ${pkgs.git}/bin/git reset --hard origin/master
       fi
       cd pkgs/applications/networking/browsers/chromium
-      ${pkgs.stdenv.shell} update.sh
+      NIX_PATH="nixpkgs=${home}" ${pkgs.stdenv.shell} update.sh
       ${pkgs.git}/bin/git commit -a -m "chromium: Automatic update" || true
     '';
 
