7714583412f112285973fca826b2ef0e8aea9af8
aszlig <aszlig@redmoonstudios.org>
2015-01-17 04:45:46 +0100

    build-erlang: Pass through libraryDir and appDir.
    The change looks bigger than it is, as we needed to reindent the
    derivation attributes in order to recursively pass the derivation into
    its own passed through attributes.
    
    So now, we shouldn't need to push all that "$foo/lib/erlang/lib" cruft
    around anymore.
    
    Signed-off-by: aszlig <aszlig@redmoonstudios.org>

--- pkgs/build-support/build-erlang.nix
+++ pkgs/build-support/build-erlang.nix
@@ -17,51 +17,53 @@ let
     inputList = flatten (map getDeps erlangDeps);
   in uniqList { inherit inputList; };
 
-  mkErlPath = drv: "${drv}/lib/erlang/lib/${drv.packageName}";
+  self = stdenv.mkDerivation ({
+    name = "${name}-${version}";
 
-in stdenv.mkDerivation ({
-  name = "${name}-${version}";
+    buildInputs = buildInputs ++ [ erlang patchedRebar ];
 
-  buildInputs = buildInputs ++ [ erlang patchedRebar ];
+    postPatch = ''
+      rm -f rebar
+      if [ -e "src/${name}.app.src" ]; then
+        sed -i \
+          -e 's/{ *vsn *,[^}]*}/{vsn, "${version}"}/' \
+          -e '/^ *%/s/[^[:print:][:space:]]/?/g' \
+          "src/${name}.app.src"
+      fi
+      ${postPatch}
+    '';
 
-  postPatch = ''
-    rm -f rebar
-    if [ -e "src/${name}.app.src" ]; then
-      sed -i \
-        -e 's/{ *vsn *,[^}]*}/{vsn, "${version}"}/' \
-        -e '/^ *%/s/[^[:print:][:space:]]/?/g' \
-        "src/${name}.app.src"
-    fi
-    ${postPatch}
-  '';
+    configurePhase = ''
+      runHook preConfigure
+      runHook postConfigure
+    '';
 
-  configurePhase = ''
-    runHook preConfigure
-    runHook postConfigure
-  '';
+    NIX_ERLANG_DEPENDENCIES = let
+      mkDepMapping = d: "${d.packageName}=${d.appDir}";
+    in concatMapStringsSep ":" mkDepMapping recursiveErlangDeps;
 
-  NIX_ERLANG_DEPENDENCIES = let
-    mkDepMapping = d: "${d.packageName}=${mkErlPath d}";
-  in concatMapStringsSep ":" mkDepMapping recursiveErlangDeps;
+    buildPhase = ''
+      runHook preBuild
+      rebar compile
+      runHook postBuild
+    '';
 
-  buildPhase = ''
-    runHook preBuild
-    rebar compile
-    runHook postBuild
-  '';
+    installPhase = ''
+      runHook preInstall
+      for reldir in ebin priv include; do
+        [ -e "$reldir" ] || continue
+        mkdir -p "$out/lib/erlang/lib/${name}"
+        cp -rt "$out/lib/erlang/lib/${name}" "$reldir"
+      done
+      runHook postInstall
+    '';
 
-  installPhase = ''
-    runHook preInstall
-    for reldir in ebin priv include; do
-      [ -e "$reldir" ] || continue
-      mkdir -p "$out/lib/erlang/lib/${name}"
-      cp -rt "$out/lib/erlang/lib/${name}" "$reldir"
-    done
-    runHook postInstall
-  '';
+    passthru = rec {
+      packageName = name;
+      libraryDir = "${self}/lib/erlang/lib";
+      appDir = "${libraryDir}/${packageName}";
+      inherit erlangDeps recursiveErlangDeps;
+    };
+  } // removeAttrs attrs [ "name" "postPatch" "buildInputs" ]);
 
-  passthru = {
-    packageName = name;
-    inherit erlangDeps recursiveErlangDeps;
-  };
-} // removeAttrs attrs [ "name" "postPatch" "buildInputs" ])
+in self
