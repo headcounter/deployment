0952b13eaa6eb20441f0f7effebbc9d7ab97185d
aszlig <aszlig@redmoonstudios.org>
2015-01-17 15:15:27 +0100

    tests/headcounter: Add subtests using xmppoke.
    Right now, this is very ugly because we don't check whether the xmppoke
    result is actually to our satisfaction. But at least now it works, even
    though it is still slow as hell.
    
    To improve the speed it's probably for the best to do one VM test per
    domain.
    
    We're also using PostgreSQL as the xmppoke database backend, where we
    connect using the root user and trust authentication. It's just within a
    VM test, so it doesn't matter whether it's really secure or not.
    
    Another workaround right now is to list the domain names within the test
    expression, so we may want to add a purpose flag to the vhost module.
    
    Signed-off-by: aszlig <aszlig@redmoonstudios.org>

--- tests/headcounter/default.nix
+++ tests/headcounter/default.nix
@@ -1,22 +1,102 @@
 import <nixpkgs/nixos/tests/make-test.nix> ({ pkgs, ... }:
 
-{
+let
+  localPkgs = import ../../pkgs {
+    inherit pkgs;
+  };
+
+  patchedPoke = pkgs.lib.overrideDerivation localPkgs.xmppoke (o: {
+    postPatch = (o.postPatch or "") + ''
+      sed -i -e '/dbi.Connect/{
+        s/"xmppoke", *"xmppoke"/"xmppoke", "root"/
+        s/"localhost"/nil/
+      }' poke.lua
+    '';
+  });
+
+  testedDomains = [ "headcounter" "aszlig" "noicq" "no_icq" ];
+
+in {
   nodes = {
-    ultron = {
+    ultron = { config, lib, ... }: with lib; {
       imports = import ../../modules/module-list.nix ++ [
         ../../xmpp.nix ../../domains.nix
       ];
+
       headcounter.useSnakeOil = true;
       users.extraUsers.mongoose.extraGroups = [ "keys" ];
+
+      headcounter.vhosts = genAttrs testedDomains (name: {
+        device = "eth1";
+      });
+
       networking.firewall.enable = false;
+      virtualisation.vlans = [ 1 ];
     };
-    benteflork = {
+
+    client = { nodes, pkgs, config, lib, ... }: with lib; let
+      inherit (nodes.ultron.config.headcounter) vhosts;
+    in {
+      networking.extraHosts = let
+        mkHostEntry = _: vhost: lib.optionalString (vhost.fqdn != null) ''
+          ${vhost.ipv4} ${vhost.fqdn}
+          ${vhost.ipv6} ${vhost.fqdn}
+        '';
+      in concatStrings (mapAttrsToList mkHostEntry vhosts);
+
       networking.firewall.enable = false;
+      virtualisation.vlans = [ 1 ];
+
+      networking.localCommands = let
+        mkRoute = _: vhost: ''
+          ip -4 route add '${vhost.ipv4}' dev eth1
+          ip -6 route add '${vhost.ipv6}' dev eth1
+        '';
+      in ''
+        ${concatStrings (mapAttrsToList mkRoute vhosts)}
+        ip -4 route flush cache
+        ip -6 route flush cache
+      '';
+
+      services.postgresql = {
+        enable = true;
+        package = pkgs.postgresql;
+        initialScript = pkgs.writeText "init.sql" ''
+          CREATE ROLE xmppoke WITH LOGIN;
+          CREATE DATABASE xmppoke OWNER xmppoke;
+          \c xmppoke
+          \i ${patchedPoke}/share/xmppoke/schema.pg.sql
+        '';
+        authentication = ''
+          local all xmppoke trust
+        '';
+      };
     };
   };
 
-  testScript = ''
+  testScript = { nodes, ... }: with pkgs.lib; let
+    inherit (nodes.ultron.config.headcounter) vhosts;
+
+    vhostTest = vhost: ''
+      $client->nest("check availability", sub {
+        $client->succeed("ping -c1 ${vhost.fqdn} >&2");
+        $client->succeed("nc -z ${vhost.fqdn} 5222");
+      });
+      $client->succeed("${patchedPoke}/bin/xmppoke ${vhost.fqdn} >&2");
+    '';
+
+    mkVhostTest = name: vhost: optionalString (elem name testedDomains) ''
+      subtest "vhost-${name}", sub {
+        ${vhostTest vhost}
+      };
+    '';
+
+  in ''
     startAll;
     $ultron->waitForUnit("mongooseim.service");
+    $client->waitForUnit("network.target");
+    $client->waitForUnit("postgresql.service");
+
+    ${concatStrings (mapAttrsToList mkVhostTest vhosts)}
   '';
 })
