8859f9ebe9d3fff2a8d30914e2deae53cedf32f5
aszlig <aszlig@redmoonstudios.org>
2015-01-20 23:25:56 +0100

    libcommuni: Update to version 3.3.0.
    One of the dependencies of Spectrum 2 to have IRC support.
    
    Signed-off-by: aszlig <aszlig@redmoonstudios.org>

--- pkgs/spectrum2/libcommuni.nix
+++ pkgs/spectrum2/libcommuni.nix
@@ -1,23 +1,19 @@
-{ stdenv, fetchurl, qt4 }:
+{ stdenv, fetchFromGitHub, qt4 }:
 
 stdenv.mkDerivation rec {
   name = "libcommuni-${version}";
-  version = "2.2.0";
+  version = "3.3.0";
 
-  src = fetchurl {
-    url = "https://github.com/communi/libcommuni/archive/v${version}.tar.gz";
-    sha256 = "1ir8kwjlfwbb87fdmki6dwmd61xraj7c0hami8yzgdz0nxi5kfsw";
+  src = fetchFromGitHub {
+    repo = "libcommuni";
+    owner = "communi";
+    rev = "v${version}";
+    sha256 = "1bsblr4rizgjin1p9igymcv40lbkm9p8f4h3k8pwdf6qfxdnzxsx";
   };
 
   postPatch = ''
-    sed -i -e 's|/bin/pwd|pwd|' -e 's/\<which\>/type -P/' configure
+    sed -i -e 's|/bin/pwd|pwd|g' -e 's/\<which\>/type -P/g' configure
   '';
-
-/* Only needed for 3.x
-  preConfigure = ''
-    configureFlags="-importdir $out/lib/qt4/imports"
-  '';
-*/
 
   propagatedBuildInputs = [ qt4 ];
 
