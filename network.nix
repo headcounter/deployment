headcounter/deployment

commit 3ae87ebf2d90746b997d5dcd0f6b4dadbda8958c

common: Split into common and common-machines.
In common.nix we're now defining things we need for *every* machine,
every VM test, basically every NixOS configuration in this repository.

Right now, this is just another import of module-list.nix and turning
off the damn netfilter, which is most of the duplication that has
infested the code base.

The common-machines.nix file is pretty much the same as the old
common.nix, except that it now imports the new common.nix as well and
thus turns off the packet filter.

Signed-off-by: aszlig <aszlig@redmoonstudios.org>

network.nix
@@ -1,7 +1,6 @@
 let
   mkMachine = attrs: attrs // {
-    imports = import modules/module-list.nix
-           ++ [ ./common.nix ]
+    imports = [ ./common-machine.nix ]
            ++ attrs.imports or [];
   };
 in {
@@ -87,10 +86,9 @@ in {
   unzervalt = { nodes, lib, ... }: {
     deployment.targetEnv = "container";
     deployment.container.host = nodes.ultron.config;
-    imports = import modules/module-list.nix
+    imports = [ ./common.nix ]
            ++ lib.optional (lib.pathExists ./private/default.nix) ./private;
     services.headcounter.webspace.enable = true;
-    networking.firewall.enable = false;
     users.mutableUsers = false;
   };
 }

common-machines.nix
@@ -1,0 +1,27 @@
+{ pkgs, config, ... }:
+{
+  imports = [ ./common.nix ];
+
+  deployment.targetEnv = "hetzner";
+
+  environment.systemPackages = with pkgs; [
+    htop iotop
+  ];
+
+  services.openntpd.enable = true;
+
+  nix = {
+    package = pkgs.nixUnstable;
+    nrBuildUsers = 100;
+    useChroot = true;
+    readOnlyStore = true;
+    extraOptions = ''
+      build-cores = 0
+    '';
+  };
+
+  nixpkgs.config.allowUnfree = true;
+  hardware.cpu.intel.updateMicrocode = true;
+
+  time.timeZone = "Europe/Berlin";
+}

common.nix
@@ -1,27 +1,4 @@
-{ pkgs, config, ... }:
 {
-  deployment.targetEnv = "hetzner";
-
+  imports = import ./modules/module-list.nix;
   networking.firewall.enable = false;
-
-  environment.systemPackages = with pkgs; [
-    htop iotop
-  ];
-
-  services.openntpd.enable = true;
-
-  nix = {
-    package = pkgs.nixUnstable;
-    nrBuildUsers = 100;
-    useChroot = true;
-    readOnlyStore = true;
-    extraOptions = ''
-      build-cores = 0
-    '';
-  };
-
-  nixpkgs.config.allowUnfree = true;
-  hardware.cpu.intel.updateMicrocode = true;
-
-  time.timeZone = "Europe/Berlin";
 }

release.nix
@@ -19,9 +19,10 @@ in {
     };
 
     modules = evalModules {
-      modules = import ./modules/module-list.nix ++ singleton {
-        options = dummyOpts;
-      };
+      modules = [
+        ./common.nix
+        { options = dummyOpts; }
+      ];
       args = {
         inherit pkgs;
       };

tests/headcounter/default.nix
@@ -14,9 +14,7 @@ let
     });
   in {
     ultron = { config, lib, ... }: with lib; {
-      imports = import ../../modules/module-list.nix ++ [
-        ../../xmpp.nix ../../domains.nix
-      ];
+      imports = [ ../../common.nix ../../xmpp.nix ../../domains.nix ];
 
       headcounter.useSnakeOil = true;
       users.extraUsers.mongoose.extraGroups = [ "keys" ];
@@ -25,7 +23,6 @@ let
         device = "eth1";
       });
 
-      networking.firewall.enable = false;
       virtualisation.vlans = [ 1 ];
     };
 
@@ -39,7 +36,6 @@ let
         '';
       in concatStrings (mapAttrsToList mkHostEntry vhosts);
 
-      networking.firewall.enable = false;
       virtualisation.vlans = [ 1 ];
 
       networking.localCommands = let

tests/mongooseim.nix
@@ -249,23 +249,21 @@ in {
 
   nodes = {
     server1 = { config, pkgs, ... }: {
-      imports = import ../modules/module-list.nix;
+      imports = [ ../common.nix ];
       services.headcounter.mongooseim = {
         enable = true;
         inherit cookie;
         settings = mkConfig server1;
       };
-      networking.firewall.enable = false;
     };
 
     server2 = { config, pkgs, ... }: {
-      imports = import ../modules/module-list.nix;
+      imports = [ ../common.nix ];
       services.headcounter.mongooseim = {
         enable = true;
         inherit cookie;
         settings = mkConfig server2;
       };
-      networking.firewall.enable = false;
     };
 
     client = {};

commit def8677fd18f982f639158aa5333bdc8d304f8f1

network: Fix reference to common-machines.nix.
Fixes a regression from 3ae87ebf2d90746b997d5dcd0f6b4dadbda8958c.

Signed-off-by: aszlig <aszlig@redmoonstudios.org>

network.nix
@@ -1,6 +1,6 @@
 let
   mkMachine = attrs: attrs // {
-    imports = [ ./common-machine.nix ]
+    imports = [ ./common-machines.nix ]
            ++ attrs.imports or [];
   };
 in {