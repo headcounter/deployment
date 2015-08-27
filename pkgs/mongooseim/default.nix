35cb99675c1424d5fee212d4aab11ca4d2cddf72
aszlig <aszlig@redmoonstudios.org>
2015-08-28 01:53:28 +0200

    pkgs/mongooseim: Pass through mainAppDir.
    This is going to be used for modules that want to reference files within
    the main application of the release. A check at the installPhase is
    there to make sure it exists should there be a new MongooseIM release
    which changes the naming scheme of that directory.
    
    Signed-off-by: aszlig <aszlig@redmoonstudios.org>

--- pkgs/mongooseim/default.nix
+++ pkgs/mongooseim/default.nix
@@ -2,60 +2,71 @@
 , erlangPackages
 }:
 
-buildErlang rec {
-  name = "mongooseim";
-  version = "1.5.1";
+let
+  mainAppDir = "lib/ejabberd-2.1.8+mim-${self.version}";
 
-  src = fetchFromGitHub {
-    owner = "esl";
-    repo = "MongooseIM";
-    rev = version;
-    sha256 = "0lpia21kmy9im1c0d8zx0i9swdiifypzj1k4xc3drp8kssz9adrz";
+  self = buildErlang rec {
+    name = "mongooseim";
+    version = "1.5.1";
+
+    src = fetchFromGitHub {
+      owner = "esl";
+      repo = "MongooseIM";
+      rev = version;
+      sha256 = "0lpia21kmy9im1c0d8zx0i9swdiifypzj1k4xc3drp8kssz9adrz";
+    };
+
+    patches = [
+      ./reltool.patch ./journald.patch ./systemd.patch
+      ./s2s-listener-certfile.patch
+    ];
+
+    postPatch = ''
+      sed -i -e '/\<vsn\>/s/{ *cmd *,[^}]*}/"2.1.8+mim-${version}"/' \
+        apps/ejabberd/src/ejabberd.app.src
+
+      sed -i \
+        -e '/lager/s/2\.0\.3/.*/' \
+        -e '/cowboy/s/0\.9\.0/.*/' \
+        rebar.config
+    '';
+
+    buildInputs = [ pam zlib openssl expat ];
+    erlangDeps = with erlangPackages; [
+      alarms base16 cowboy cuesport ecoveralls exml folsom fusco idna lager
+      mochijson2 mustache p1_cache_tab p1_stringprep pa proper recon redo seestar
+    ];
+
+    postBuild = ''
+      rebar generate
+
+      # Remove nodetool and the erl wrapper, because we don't need it
+      # but be extra paranoid about the deletion. We want deletion to
+      # fail if there are extra files left or if a file does not exist.
+      basedir="$(echo "rel/${name}"/erts-[0-9]*)"
+      for to_delete in nodetool erl; do
+        rm "$basedir/bin/$to_delete" || exit 1
+      done
+      rmdir "$basedir/bin" || exit 1
+      rmdir "$basedir" || exit 1
+    '';
+
+    installPhase = ''
+      cp -a "rel/${name}" "$out"
+      if [ ! -d "$out/${mainAppDir}" ]; then
+        echo "$out/${mainAppDir} does not exist!" >&2
+        exit 1
+      fi
+    '';
+
+    passthru.mainAppDir = "${self}/${mainAppDir}";
+
+    meta = {
+      homepage = "https://www.erlang-solutions.com/products/"
+               + "mongooseim-massively-scalable-ejabberd-platform";
+      description = "An Ejabberd fork utilizing Erlang/OTP features.";
+      license = stdenv.lib.licenses.gpl2Plus;
+    };
   };
 
-  patches = [
-    ./reltool.patch ./journald.patch ./systemd.patch
-    ./s2s-listener-certfile.patch
-  ];
-
-  postPatch = ''
-    sed -i -e '/\<vsn\>/s/{ *cmd *,[^}]*}/"2.1.8+mim-${version}"/' \
-      apps/ejabberd/src/ejabberd.app.src
-
-    sed -i \
-      -e '/lager/s/2\.0\.3/.*/' \
-      -e '/cowboy/s/0\.9\.0/.*/' \
-      rebar.config
-  '';
-
-  buildInputs = [ pam zlib openssl expat ];
-  erlangDeps = with erlangPackages; [
-    alarms base16 cowboy cuesport ecoveralls exml folsom fusco idna lager
-    mochijson2 mustache p1_cache_tab p1_stringprep pa proper recon redo seestar
-  ];
-
-  postBuild = ''
-    rebar generate
-
-    # Remove nodetool and the erl wrapper, because we don't need it
-    # but be extra paranoid about the deletion. We want deletion to
-    # fail if there are extra files left or if a file does not exist.
-    basedir="$(echo "rel/${name}"/erts-[0-9]*)"
-    for to_delete in nodetool erl; do
-      rm "$basedir/bin/$to_delete" || exit 1
-    done
-    rmdir "$basedir/bin" || exit 1
-    rmdir "$basedir" || exit 1
-  '';
-
-  installPhase = ''
-    cp -a "rel/${name}" "$out"
-  '';
-
-  meta = {
-    homepage = "https://www.erlang-solutions.com/products/"
-             + "mongooseim-massively-scalable-ejabberd-platform";
-    description = "An Ejabberd fork utilizing Erlang/OTP features.";
-    license = stdenv.lib.licenses.gpl2Plus;
-  };
-}
+in self
