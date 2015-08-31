cf9f7f244aba5ce5e2ec9c06272929269151c347
aszlig <aszlig@redmoonstudios.org>
2015-08-31 20:11:33 +0200

    build-erlang: Split off rewrite-rebar-config.
    Having this in an external file makes development on it much easier as
    we have syntax highlighting and indentation.
    
    Signed-off-by: aszlig <aszlig@redmoonstudios.org>

--- pkgs/build-support/build-erlang/default.nix
+++ pkgs/build-support/build-erlang/default.nix
@@ -24,25 +24,8 @@ let
     buildInputs = buildInputs ++ [ erlang patchedRebar ];
 
     postPatch = ''
-      "${writeEscript "rewrite-rebar-config" [] ''
-        main(Files) ->
-          Rewrite = fun(File) ->
-            try
-              {ok, Original} = file:consult(File),
-              {deps, OrigDeps} = lists:keyfind(deps, 1, Original),
-              NewDeps = [{Name, ".*", Src} || {Name, _, Src} <- OrigDeps],
-              Rewritten = lists:keyreplace(deps, 1, Original, {deps, NewDeps}),
-              PPrint = fun(T) ->
-                io_lib:format("~tp.~n", [T])
-              end,
-              Data = lists:map(PPrint, Rewritten),
-              file:write_file(File, Data)
-            catch
-              _:_ -> {error, ignored}
-            end
-          end,
-          lists:map(Rewrite, Files).
-      ''}" rebar.config rebar.config.script || :
+      "${writeEscript "rewrite-rebar-config" [] ./rewrite-rebar-config.erl}" \
+        rebar.config rebar.config.script || :
 
       subappfiles="$(
         for subapp in apps/*; do
---
+++ pkgs/build-support/build-erlang/rewrite-rebar-config.erl
@@ -0,0 +1,17 @@
+do_rewrite(File) ->
+    try
+        {ok, Original} = file:consult(File),
+        {deps, OrigDeps} = lists:keyfind(deps, 1, Original),
+        NewDeps = [{Name, ".*", Src} || {Name, _, Src} <- OrigDeps],
+        Rewritten = lists:keyreplace(deps, 1, Original, {deps, NewDeps}),
+        PPrint = fun(T) ->
+            io_lib:format("~tp.~n", [T])
+        end,
+        Data = lists:map(PPrint, Rewritten),
+        file:write_file(File, Data)
+    catch
+        _:_ -> {error, ignored}
+    end.
+
+main(Files) ->
+    lists:map(fun do_rewrite/1, Files).
