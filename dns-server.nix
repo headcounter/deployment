22f03d87622a6ed2f834005181276f07d257c95a
aszlig <aszlig@redmoonstudios.org>
2015-09-01 03:14:25 +0200

    dns-server: Update DNS config to new XMPP setting.
    Now that our old server (merkelig) has been shut down, we can safely
    remove it from our DNS configuration, along with some unused transport
    aliases.
    
    Unfortunately, I didn't have the time to properly implement this with a
    function that maps over nodes and automatically discovers all vhosts.
    
    But for now, this should do it.
    
    Signed-off-by: aszlig <aszlig@redmoonstudios.org>

--- dns-server.nix
+++ dns-server.nix
@@ -1,4 +1,4 @@
-{ lib, ... }:
+{ lib, nodes, ... }:
 
 {
   deployment.hetzner.partitions = ''
@@ -38,13 +38,6 @@
       pubsub      IN  CNAME ${dest}
       vjud        IN  CNAME ${dest}
       icq         IN  CNAME ${dest}
-      aim         IN  CNAME ${dest}
-      msn         IN  CNAME ${dest}
-      yahoo       IN  CNAME ${dest}
-      mail        IN  CNAME ${dest}
-      jmc         IN  CNAME ${dest}
-      gadugadu    IN  CNAME ${dest}
-      sms         IN  CNAME ${dest}
       proxy       IN  CNAME ${dest}
 
       _jabber._tcp      IN SRV 10 0 5269 ${dest}
@@ -52,25 +45,30 @@
       _xmpp-client._tcp IN SRV 10 0 5222 ${dest}
     '';
 
+    # XXX: Don't hardcode ultron here!
+    inherit (nodes.ultron.config.headcounter) vhosts;
+
   in {
     enable = true;
     interfaces = lib.mkForce []; # all interfaces
     zones = {
       "aszlig.net.".data = mkZone "aszlig.net" ''
         ; Generic stuff
-        @      IN  A     88.198.198.218
-        @      IN  AAAA  2001:6f8:900:72a::2
-        jabber IN  A     88.198.43.4
+        @      IN  A     ${vhosts.aszlig.ipv4}
+        @      IN  AAAA  ${vhosts.aszlig.ipv6}
 
         ; Legacy DNS servers
         ns1 IN A 85.10.206.212
         ns2 IN A 217.20.112.88
 
-        ; @ is legacy server
         ${mkXMPPRecords "@"}
       '';
 
       "headcounter.org.".data = mkZone "headcounter.org" ''
+        ; Generic stuff
+        @           IN  A     ${vhosts.headcounter.ipv4}
+        @           IN  AAAA  ${vhosts.headcounter.ipv6}
+
         ; Hosts of the new deployment
         taalo      IN A 144.76.61.117
         benteflork IN A 144.76.202.147
@@ -82,39 +80,22 @@
         ns2 IN A    78.47.142.38
         ns2 IN AAAA 2a01:4f8:d13:5308::2
 
-        ; Legacy transition stuff
-        @           IN  A     78.47.32.129
-        @           IN  AAAA  2a01:4f8:162:4187::1
-        merkelig    IN  A     88.198.198.219
-        merkelig    IN  AAAA  2001:6f8:900:72a::2
-        jabber      IN  A     88.198.43.4
-        jabber      IN  AAAA  2001:6f8:900:72a::2
-        www         IN  A     88.198.43.4
-        www         IN  AAAA  2001:6f8:900:72a::2
-
-        ; "merkelig" is legacy server
-        ${mkXMPPRecords "merkelig"}
+        ${mkXMPPRecords "@"}
       '';
 
       "no-icq.org.".data = mkZone "no-icq.org" ''
         ; Generic stuff
-        @      IN  A    88.198.198.220
-        @      IN  AAAA 2001:6f8:900:72a::2
-        jabber IN  A    88.198.43.4
-        www    IN  A    88.198.43.4
+        @      IN  A    ${vhosts.no_icq.ipv4}
+        @      IN  AAAA ${vhosts.no_icq.ipv6}
 
-        ; @ is legacy server
         ${mkXMPPRecords "@"}
       '';
 
       "noicq.org.".data = mkZone "noicq.org" ''
         ; Generic stuff
-        @      IN A    88.198.198.221
-        @      IN AAAA 2001:6f8:900:72a::2
-        jabber IN A    88.198.43.4
-        www    IN A    88.198.43.4
+        @      IN  A    ${vhosts.noicq.ipv4}
+        @      IN  AAAA ${vhosts.noicq.ipv6}
 
-        ; @ is legacy server
         ${mkXMPPRecords "@"}
       '';
     };
