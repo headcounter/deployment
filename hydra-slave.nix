866048e43342b4b608fc62c11b451c88ef9625d7
aszlig <aszlig@redmoonstudios.org>
2015-10-24 15:29:59 +0200

    hydra-slave: Remove nixosSSHKey.
    This was from the time where benteflork was part of the official Hydra
    and as it's no longer the case, we can safely drop it.
    
    Signed-off-by: aszlig <aszlig@redmoonstudios.org>

--- hydra-slave.nix
+++ hydra-slave.nix
@@ -1,18 +1,6 @@
 { resources, ... }:
 
 let
-  nixosSSHKey = "ssh-dss "
-    + "AAAAB3NzaC1kc3MAAACBAMHRjGSDaBp4Z30JF4S9ApabBCpdr57Ad0aD9oH2A"
-    + "/WEFnWYQSAzK4E/HHD2DV2XP1stNkZ1ks2v3F4Yu/veR+qVlUWbJW1RIIfuQg"
-    + "kG44K0R3C2qx4BAZUVYzju1NVCJbBOO6ipVY9cfmpokV52HZFhP/2HocTNLoa"
-    + "v3F0AsbbJAAAAFQDaJiQdpJBEa4Wr5FfVl1kYqmQZJwAAAIEAwbern5XL+SNI"
-    + "Ma+sJ3CBhrWyYExYWiUbdmhQEfyEAUmoPsEr1qpb+0WREic9Nrxz48QWZDK5x"
-    + "MvzZyQEkuAMJUBWcdm12rME7WMvg7OZGr9DADjAtfMfj3Ui2XvOuQ3ia/OTsM"
-    + "GkQTDWnkOM9Ni128SNSl9urFBlXATdGvo+468AAACBAK8s6LddhhkRqsF/l/L"
-    + "2ooS8c8A1rTFWAOy3/sgXFNvMyS/Mig2p966xRrRHr7Bc+H2SuKEE5WmLCXqy"
-    + "mgxLHhrFU4zm/W/ej1yB1CAThd4xUfgJu4touJROjvcD1zzlmLeat0fp2k5mC"
-    + "uiLKcTKi0vxKWiiopF9nvBBK+7ODPC7 buildfarm@nixos";
-
   nixosSigningKey = ''
     -----BEGIN PUBLIC KEY-----
     MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAvXmUhovC36OJ9a/3oTaV
@@ -51,7 +39,6 @@ in {
     createHome = true;
     openssh.authorizedKeys.keys = [
       resources.sshKeyPairs."hydra-build".publicKey
-      nixosSSHKey
     ];
   };
 
