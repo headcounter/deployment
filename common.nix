75cc24c484525970e1924981873bb2ef0a06f983
aszlig <aszlig@redmoonstudios.org>
2015-10-26 13:00:35 +0100

    common: Revert "Use Erlang R17 by default".
    This reverts commit d21d5dc6051eabe7e99756a69bb2bb6f16a048e9.
    
    Since the introduction of MongooseIM 1.6.0 we're going to aim for Erlang
    R18, so we no longer need this override.
    
    Signed-off-by: aszlig <aszlig@redmoonstudios.org>

--- common.nix
+++ common.nix
@@ -4,8 +4,6 @@
 
   nixpkgs.config.packageOverrides = pkgs: {
     inherit (import ./pkgs { inherit pkgs; }) headcounter;
-    # XXX: This is needed for MongooseIM < 1.6.0 for now
-    erlang = pkgs.erlangR17;
   };
 
   nix.extraOptions = ''
