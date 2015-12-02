56b22d972323540c1e3dc2c964ab6f8044d096a8
aszlig <aszlig@redmoonstudios.org>
2015-12-02 19:13:31 +0100

    common: Set journal max retention to 3 months.
    This avoids that the journal is getting too large for historic stuff we
    don't need anyway.
    
    Signed-off-by: aszlig <aszlig@redmoonstudios.org>

--- common.nix
+++ common.nix
@@ -2,6 +2,10 @@
   imports = import ./modules/module-list.nix;
   networking.firewall.enable = false;
 
+  services.journald.extraConfig = ''
+    MaxRetentionSec=3month
+  '';
+
   nixpkgs.config.packageOverrides = pkgs: {
     inherit (import ./pkgs { inherit pkgs; }) headcounter;
     # XXX: This is needed for MongooseIM < 1.6.0 for now
