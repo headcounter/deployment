headcounter/deployment

commit fa7e9d828a279e8792666a213d1040a2979cdcef

services/mongooseim: Add listener options for HTTP
So far it has been a bit annoying to configure HTTP/cowboy modules:

  options.modules = [
    { tuple = [
        server1
        "/api"
        { atom = "mongoose_api"; }
        { handlers = [
            { atom = "mongoose_api_metrics"; }
            { atom = "mongoose_api_users"; }
          ];
        }
      ];
    }
    { tuple = ["_" "/http-bind" { atom = "mod_bosh"; }]; }
    { tuple = ["_" "/ws-xmpp"   { atom = "mod_websockets"; }]; }
  ];

This now should be come something like this:

  http.modules = [
    { host = server1;
      path = "/api";
      handler = "mongoose_api";
      options.handlers = [
        { atom = "mongoose_api_metrics"; }
        { atom = "mongoose_api_users"; }
      ];
    }
    { path = "/http-bind";
      handler = "mod_bosh";
    }
    { path = "/ws-xmpp";
      handler = "mod_websockets";
    }
  ];

While this doesn't make a lot of difference in terms of the line count
it is less nested and we have type safety on top of it.

Note that I removed the "mongoose_api" handlers with this commit, even
though I used it as an example. The reason for removing it is that it's
deprecated by the upstream project.

Signed-off-by: aszlig <aszlig@redmoonstudios.org>

tests/mongooseim/default.nix
@@ -210,58 +210,33 @@ let
         options.max_stanza_size = 65536;
       }
       { port = 5280;
-        module = "ejabberd_cowboy";
-        options.num_acceptors = 10;
-        options.max_connections = 1024;
-        options.modules = [
-          { tuple = [
-              server1
-              "/api"
-              { atom = "mongoose_api"; }
-              { handlers = [
-                  { atom = "mongoose_api_metrics"; }
-                  { atom = "mongoose_api_users"; }
-                ];
-              }
-            ];
+        http.enable = true;
+        http.modules = [
+          { path = "/http-bind";
+            handler = "mod_bosh";
           }
-          { tuple = ["_" "/http-bind" { atom = "mod_bosh"; }]; }
-          { tuple = [
-              "_"
-              "/ws-xmpp"
-              { atom = "mod_websockets"; }
-              { ejabberd_service = {
-                  access.atom = "all";
-                  shaper_rule.atom = "fast";
-                  password = "secret";
-                };
-              }
-            ];
+          { path = "/ws-xmpp";
+            handler = "mod_websockets";
           }
         ];
+        options.num_acceptors = 10;
+        options.max_connections = 1024;
       }
       { port = 5285;
-        module = "ejabberd_cowboy";
+        http.enable = true;
+        http.modules = [
+          { path = "/http-bind";
+            handler = "mod_bosh";
+          }
+          { path = "/ws-xmpp";
+            handler = "mod_websockets";
+          }
+        ];
         options.num_acceptors = 10;
         options.max_connections = 1024;
         options.ssl.certfile = toString pubKeyFile;
         options.ssl.keyfile = toString privKeyFile;
         options.ssl.password = "";
-        options.modules = [
-          { tuple = [
-              server1
-              "/api"
-              { atom = "mongoose_api"; }
-              { handlers = [
-                  { atom = "mongoose_api_metrics"; }
-                  { atom = "mongoose_api_users"; }
-                ];
-              }
-            ];
-          }
-          { tuple = ["_" "/http-bind" { atom = "mod_bosh"; }]; }
-          { tuple = ["_" "/ws-xmpp"   { atom = "mod_websockets"; }]; }
-        ];
       }
       { port = 5269;
         module = "ejabberd_s2s_in";

modules/services/mongooseim/listeners.nix
@@ -2,9 +2,90 @@
 
 { lib, config, ... }:
 
-with lib;
+let
+  inherit (lib) mkOption types;
 
-{
+  cowboyDoc = "https://ninenines.eu/docs/en/cowboy/1.0/guide";
+
+  httpModule.options = {
+    host = mkOption {
+      type = types.nullOr types.str;
+      default = null;
+      example = "example.org";
+      description = ''
+        The virtual host name to serve requests for the associated
+        <option>handler</option>.
+
+        If the value is <literal>null</literal> any host is accepted.
+
+        This allows to use the <link
+        xlink:href="${cowboyDoc}/routing/#match_syntax">host match syntax of
+        the Cowboy web server</link>.
+      '';
+    };
+
+    path = mkOption {
+      type = types.str;
+      default = "/";
+      example = "/api/rooms/:id/users/[:user]";
+      description = let
+      in ''
+        The path part of the URL to map to the associated
+        <option>handler</option>.
+
+        This allows to use the <link
+        xlink:href="${cowboyDoc}/routing/#match_syntax">path match syntax of
+        the Cowboy web server</link>.
+      '';
+    };
+
+    handler = mkOption {
+      type = types.str;
+      example = "mod_bosh";
+      description = ''
+        The handler module to use for serving requests, which is any Cowboy
+        module, not only the ones that come with MongooseIM.
+
+        Handlers that come with the MongooseIM source tree:
+
+        <simplelist type="inline">
+        ${lib.concatMapStrings (handler: ''
+          <member><literal>${handler}</literal></member>
+        '') [
+          /* Retrieved using the following command on the source tree:
+
+             grep 'cowboy\|^routes' apps/ejabberd/src/*.erl | sed -r \
+               -e '/\/((ejabberd|mod)_cowboy|mongoose_api(_common)?)\.erl:/d' \
+               -e '/^[^:]*: *%/d' -e 's/^.*\/([^\/]+)\.erl:.*$/"\1"/' | sort -u
+          */
+          "mod_bosh"
+          "mod_revproxy"
+          "mod_websockets"
+          "mongoose_api_admin"
+          "mongoose_api_client"
+          "mongoose_api_metrics"
+          "mongoose_api_users"
+          "mongoose_client_api"
+          "mongoose_client_api_messages"
+          "mongoose_client_api_rooms"
+          "mongoose_client_api_rooms_messages"
+          "mongoose_client_api_rooms_users"
+        ]}
+        </simplelist>
+      '';
+    };
+
+    options = mkOption {
+      type = hclib.types.erlPropList;
+      default = {};
+      example.mongoose_client_api_sse.flag = true;
+      description = ''
+        Additional options to provide to the <option>handler</option>.
+      '';
+    };
+  };
+
+in {
   options = {
     port = mkOption {
       type = types.int;
@@ -41,6 +122,37 @@ with lib;
       description = "Options for the corresponding <option>module</option>.";
     };
 
+    http = {
+      enable = mkOption {
+        type = types.bool;
+        default = false;
+        example = true;
+        description = ''
+          Whether this listener module should use
+          <literal>ejabberd_cowboy</literal> for HTTP(S).
+        '';
+      };
+
+      modules = mkOption {
+        type = types.listOf (types.submodule httpModule);
+        default = [];
+        example = [
+          { host = "localhost";
+            path = "/api";
+            handler = "mongoose_api_admin";
+          }
+          { path = "/api";
+            handler = "lasse_handler";
+            options.mongoose_client_api_sse.flag = true;
+          }
+        ];
+        description = ''
+          A list of HTTP(S) modules (using <literal>ejabberd_cowboy</literal>)
+          to enable for this listener.
+        '';
+      };
+    };
+
     generatedConfig = mkOption {
       type = types.nullOr types.str;
       default = null;
@@ -49,12 +161,27 @@ with lib;
     };
   };
 
-  config.generatedConfig = let
-    addrTerm = hclib.parseErlIpAddr config.address;
-    addrSpec = singleton (hclib.erlInt config.port)
-            ++ optional (config.address != null) addrTerm
-            ++ optional (config.type != null) (hclib.erlAtom config.type);
-    addr = if length addrSpec == 1 then head addrSpec
-           else "{${concatStringsSep ", " addrSpec}}";
-  in "{${addr}, ${hclib.erlAtom config.module}, ${config.options}}";
+  config = lib.mkMerge [
+    (lib.mkIf config.http.enable {
+      module = "ejabberd_cowboy";
+      options.modules = map (module: {
+        tuple = [
+          (if module.host == null then "_" else module.host)
+          module.path
+          { atom = module.handler; }
+          { __raw = module.options; }
+        ];
+      }) config.http.modules;
+    })
+    { generatedConfig = let
+        inherit (hclib) parseErlIpAddr erlInt erlAtom;
+        addrTerm = parseErlIpAddr config.address;
+        addrSpec = lib.singleton (erlInt config.port)
+                ++ lib.optional (config.address != null) addrTerm
+                ++ lib.optional (config.type != null) (erlAtom config.type);
+        addr = if lib.length addrSpec == 1 then lib.head addrSpec
+               else "{${lib.concatStringsSep ", " addrSpec}}";
+      in "{${addr}, ${erlAtom config.module}, ${config.options}}";
+    }
+  ];
 }

commit 396ebc1ad9702cce2623f4db13d79b3a5b9448bc

tests/mongooseim: Re-add ejabberd_service handler
Regression introduced by fa7e9d828a279e8792666a213d1040a2979cdcef.

I've accidentally removed the ejabberd_service options in my quest to
remove all obsolete options. This option however is not obsolete and
actually needed by some tests.

Signed-off-by: aszlig <aszlig@redmoonstudios.org>

tests/mongooseim/default.nix
@@ -217,6 +217,11 @@ let
           }
           { path = "/ws-xmpp";
             handler = "mod_websockets";
+            options.ejabberd_service = {
+              access.atom = "all";
+              shaper_rule.atom = "fast";
+              password = "secret";
+            };
           }
         ];
         options.num_acceptors = 10;