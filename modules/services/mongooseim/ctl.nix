headcounter/deployment

commit a03502df6f94ea432ea8ecd64cbdd19225c44d2a

services/mongooseim/ctl: Allow to enter shell
In the upstream shell script there is "mongooseimctl debug" which we
didn't have so far and it's a major nuisance having to pick the cookie
and other VM args from the systemd unit file every time I need an Erlang
shell on the production system.

Signed-off-by: aszlig <aszlig@redmoonstudios.org>

modules/services/mongooseim/ctl.nix
@@ -8,18 +8,32 @@ let
 
   ctlArgsFile = pkgs.writeText "ctl.args" ''
     -setcookie ${hclib.shErlEsc hclib.erlAtom cfg.cookie}
-    -noinput
-    -hidden
     -pa ${hclib.shErlEsc id "${package.mainAppDir}/ebin"}
-    -s ejabberd_ctl
   '';
 
   mongooseimctl = pkgs.writeScriptBin "mongooseimctl" ''
     #!${pkgs.stdenv.shell}
-    exec ${pkgs.erlang}/bin/erl \
-      -args_file "${ctlArgsFile}" \
-      -sname "ctl$$@${cfg.ctlHost}" \
-      -extra "${cfg.destNodeName}" "$@"
+    if [ "$1" = debug ]; then
+      shift
+      # If TERM is unset or "dumb" -remsh doesn't connect to the remote node.
+      [ -z "$TERM" -o "$TERM" = dumb ] && export TERM=vt100
+      exec ${pkgs.erlang}/bin/erl \
+        -args_file ${lib.escapeShellArg ctlArgsFile} \
+        -sname "ctl$$@"${lib.escapeShellArg cfg.ctlHost} \
+        -hidden -remsh ${lib.escapeShellArg cfg.destNodeName} "$@"
+    fi
+    ${pkgs.erlang}/bin/erl \
+      -args_file ${lib.escapeShellArg ctlArgsFile} \
+      -sname "ctl$$@"${lib.escapeShellArg cfg.ctlHost} \
+      -noinput -hidden -s ejabberd_ctl \
+      -extra ${lib.escapeShellArg cfg.destNodeName} "$@"
+    retcode=$?
+    if [ $retcode -eq 2 -o $retcode -eq 3 ]; then
+      echo
+      echo "Use \`mongooseimctl debug' to enter an interactive Erlang shell" \
+           "of a running MongooseIM node."
+      exit $retcode
+    fi
   '';
 
 in {

commit 5164d3f38f9d54f66e975745daa8035966a0cb9c

services/mongooseim/ctl: Fix propagating exit code
Regression introduced by a03502df6f94ea432ea8ecd64cbdd19225c44d2a.

The problem was that the exit code only was preserved whenever the
mongooseimctl tool reported error codes for usage errors.

Signed-off-by: aszlig <aszlig@redmoonstudios.org>

modules/services/mongooseim/ctl.nix
@@ -36,8 +36,8 @@ let
       echo
       echo "Use \`mongooseimctl debug' to enter an interactive Erlang shell" \
            "of a running MongooseIM node."
-      exit $retcode
     fi
+    exit $retcode
   '';
 
 in {