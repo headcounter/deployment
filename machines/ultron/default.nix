55001610ba6a0d1889c1ecd4f3c76daab8cdc411
aszlig <aszlig@redmoonstudios.org>
2017-02-24 19:14:08 +0100

    ultron: Move partition information to network.nix
    This is really NixOps specific stuff like deployment.targetEnv and thus
    should belong to network.nix instead of the machine configuration if we
    want to run proper VM tests on ultron.
    
    Signed-off-by: aszlig <aszlig@redmoonstudios.org>

--- machines/ultron/default.nix
+++ machines/ultron/default.nix
@@ -44,18 +44,6 @@ let
 in {
   imports = [ ../../domains.nix ../../xmpp.nix ];
 
-  deployment.hetzner.partitions = ''
-    clearpart --all --initlabel --drives=sda,sdb
-
-    part swap1 --recommended --label=swap1 --fstype=swap --ondisk=sda
-    part swap2 --recommended --label=swap2 --fstype=swap --ondisk=sdb
-
-    part btrfs.1 --grow --ondisk=sda
-    part btrfs.2 --grow --ondisk=sdb
-
-    btrfs / --data=1 --metadata=1 --label=root btrfs.1 btrfs.2
-  '';
-
   boot.kernelPackages = pkgs.linuxPackages_latest;
 
   fileSystems."/".options = [
--- network.nix
+++ network.nix
@@ -14,6 +14,18 @@ in {
     deployment.hetzner.mainIPv4 = "5.9.105.142";
     deployment.encryptedLinksTo = [ "dugee" "gussh" ];
 
+    deployment.hetzner.partitions = ''
+      clearpart --all --initlabel --drives=sda,sdb
+
+      part swap1 --recommended --label=swap1 --fstype=swap --ondisk=sda
+      part swap2 --recommended --label=swap2 --fstype=swap --ondisk=sdb
+
+      part btrfs.1 --grow --ondisk=sda
+      part btrfs.2 --grow --ondisk=sdb
+
+      btrfs / --data=1 --metadata=1 --label=root btrfs.1 btrfs.2
+    '';
+
     services.openssh.extraConfig = lib.mkAfter ''
       ListenAddress ${config.deployment.hetzner.mainIPv4}
       ListenAddress [2a01:4f8:162:4187::]
