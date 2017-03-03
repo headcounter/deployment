fb1da70a1e7f88e0c9cb69e38f900e1b261b823e
aszlig <aszlig@redmoonstudios.org>
2017-03-03 02:28:47 +0100

    tests/headcounter/escalus: Register a few users
    We want to do blackbox testing of the XMPP deployment, so we need some
    pre-existing users.
    
    Escalus has functionality to do that but it requires direct access to
    the running MongooseIM instance (either locally or via distributed
    Erlang), so we're running a series of mongooseimctl calls on the host to
    ensure Escalus has *no* means to access the node at all.
    
    Signed-off-by: aszlig <aszlig@redmoonstudios.org>

--- tests/headcounter/per-vhost/escalus/default.nix
+++ tests/headcounter/per-vhost/escalus/default.nix
@@ -9,15 +9,26 @@ vhost:
 
   users = {
     outsider.password = "truly secure, no?";
+    outsider.shouldExist = false;
+
+    admin.host = "aszlig.net";
+    admin.password = "big admin";
+
+    wallop.host = "aszlig.net";
+    wallop.password = "small wallop";
+
+    toradmin.host = "torservers.net";
+    toradmin.password = "torservers dedicated admin";
   };
 
   mkTestConfig = hclib: fqdn: let
     mkSpecVal = val: if lib.isString val then { binary = val; } else val;
     mkUser = name: spec: {
       username.binary = name;
-      server.binary = fqdn;
-      host.binary = fqdn;
+      server.binary = spec.host or fqdn;
+      host.binary = spec.server or fqdn;
       starttls.atom = "required";
+      auth_method.binary = "SCRAM-SHA-1";
     } // lib.mapAttrs (lib.const mkSpecVal) spec;
   in pkgs.writeText "test.config" ''
     {escalus_xmpp_server, escalus_mongooseim}.
@@ -25,6 +36,16 @@ vhost:
 
     {escalus_users, ${hclib.erlPropList (lib.mapAttrs mkUser users)}}.
   '';
+
+  registerUser = fqdn: name: spec: let
+    user = spec.username or name;
+    host = spec.host or fqdn;
+    inherit (spec) password;
+    shouldExist = spec.shouldExist or true;
+    cmd = [ "mongooseimctl" "register" name host password ];
+    shellCmd = lib.concatMapStringsSep " " lib.escapeShellArg cmd;
+    perlCmd = "$ultron->succeed('${lib.escape ["'"] shellCmd}');";
+  in lib.optionalString shouldExist perlCmd;
 
   testRunner = [
     "${pkgs.erlang}/bin/ct_run"
@@ -39,7 +60,7 @@ vhost:
 
   sourceTree = pkgs.runCommand "test-source-tree" {} ''
     mkdir -p "$out/test"
-    cp ${./headcounter_SUITE.erl} "$out/test/headcounter_SUITE.erl"
+    cp "${./headcounter_SUITE.erl}" "$out/test/headcounter_SUITE.erl"
   '';
 
 in {
@@ -51,7 +72,11 @@ in {
     environment.etc."headcounter/test.config".source = mkTestConfig hclib fqdn;
   };
 
-  testScript = ''
+  testScript = { nodes, ... }: let
+    inherit (nodes.ultron.config.headcounter.vhosts.${vhost}) fqdn;
+  in ''
+    ${lib.concatStrings (lib.mapAttrsToList (registerUser fqdn) users)}
+
     $client->succeed('cp -Lr ${sourceTree}/* .');
 
     ${(import ../../../mongooseim/lib.nix {
