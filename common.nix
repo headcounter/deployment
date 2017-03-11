f7ccc1e0c925609a7014eee169f61d332c0a7406
aszlig <aszlig@redmoonstudios.org>
2017-03-11 14:29:53 +0100

    common.nix: Add kernel patch fixing tun poll()
    The patch is currently in testing and will be submitted by Hannes
    Frederic Sowa to the netdev mailing list, so this patch is preliminary.
    
    I've added an assert on NixOS release 16.09, so that this will
    deliberately break eval as soon as we switch to 17.03 because first of
    all we will have boot.kernelPatches then and it's also very likely that
    this patch isn't needed anymore.
    
    Signed-off-by: aszlig <aszlig@redmoonstudios.org>

--- common.nix
+++ common.nix
@@ -1,12 +1,36 @@
+{ config, lib, pkgs, ... }:
+
 {
   imports = import ./modules/module-list.nix;
-  networking.firewall.enable = false;
 
-  services.journald.extraConfig = ''
-    MaxRetentionSec=3month
-  '';
+  options = {
+    boot.kernelPackages = let
+      tunPatch = {
+        name = "tun-poll-fix";
+        patch = pkgs.fetchurl {
+          url = "https://redmoonstudios.org/~aszlig/fdbc/tun2.patch";
+          sha256 = "0axaacmwfy4w9m0gsz8855a98li3akidxgxh5zc4kjr4b5m2al5m";
+        };
+      };
+    in assert config.system.nixosRelease == "16.09"; lib.mkOption {
+      apply = kernelPackages: let
+        newKernel = kernelPackages.kernel.override {
+          kernelPatches = kernelPackages.kernel.kernelPatches
+                       ++ lib.singleton tunPatch;
+        };
+      in pkgs.linuxPackagesFor newKernel config.boot.kernelPackages;
+    };
+  };
 
-  nixpkgs.config.packageOverrides = pkgs: {
-    inherit (import ./pkgs { inherit pkgs; }) headcounter;
+  config = {
+    networking.firewall.enable = false;
+
+    services.journald.extraConfig = ''
+      MaxRetentionSec=3month
+    '';
+
+    nixpkgs.config.packageOverrides = pkgs: {
+      inherit (import ./pkgs { inherit pkgs; }) headcounter;
+    };
   };
 }
