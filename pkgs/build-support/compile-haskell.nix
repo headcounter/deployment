03ca5d27a9fd1c37c563c71ca4f2dc00ed1b7798
aszlig <aszlig@redmoonstudios.org>
2017-04-28 07:17:05 +0200

    compile-haskell: Allow to add drvs as buildDepends
    So far we're only using strings, which are used as attribute keys to
    look up on haskellPackages. But in order to be able to use our shiny new
    nexus package, we either would need to override haskellPackages and
    inject nexus there or provide a way to pass it directly to
    compileHaskell.
    
    I choose the latter one, because it is simpler.
    
    Signed-off-by: aszlig <aszlig@redmoonstudios.org>

--- pkgs/build-support/compile-haskell.nix
+++ pkgs/build-support/compile-haskell.nix
@@ -2,9 +2,11 @@
 
 { name, source, ghcflags ? [], buildDepends ? [] }:
 
-runCommand name {
+let
+  mkDep = hpkgs: dep: if lib.isString dep then hpkgs.${dep} else dep;
+in runCommand name {
   src = if lib.isString source then writeText "${name}.hs" source else source;
-  ghc = haskellPackages.ghcWithPackages (p: map (d: p.${d}) buildDepends);
+  ghc = haskellPackages.ghcWithPackages (p: map (mkDep p) buildDepends);
 } ''
   "$ghc/bin/ghc" ${lib.concatMapStringsSep " " lib.escapeShellArg ghcflags} \
     --make "$src" -odir . -o "$out"
