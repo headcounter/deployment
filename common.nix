1346e876b3ccf4cbf099fe40de866e3b321137ae
aszlig <aszlig@redmoonstudios.org>
2017-05-15 09:57:42 +0200

    common.nix: Revert adding kernel tun poll fix
    This reverts the following commits:
    
      * f7ccc1e0c925609a7014eee169f61d332c0a7406:
        Add kernel patch fixing tun poll()
      * 00d0790009bb9fe9313765c3cf30485d5b771d31:
        Update URL of the tun-poll-fix.patch
    
    We're going to head for release-17.03 now, so the patch is no longer
    needed, because a new set of stable kernels got released and they
    contain that patch already.
    
    Signed-off-by: aszlig <aszlig@redmoonstudios.org>

--- common.nix
+++ common.nix
@@ -1,37 +1,12 @@
-{ config, lib, pkgs, ... }:
-
 {
   imports = import ./modules/module-list.nix;
+  networking.firewall.enable = false;
 
-  options = {
-    boot.kernelPackages = let
-      tunPatch = rec {
-        name = "tun-poll-fix";
-        patch = pkgs.fetchurl {
-          name = "${name}.patch";
-          url = "https://patchwork.ozlabs.org/patch/737900/raw/";
-          sha256 = "18330picypxbaq7and7zjjv6pkmxp1fhc200dzn6ajamnzyfy2p9";
-        };
-      };
-    in assert config.system.nixosRelease == "16.09"; lib.mkOption {
-      apply = kernelPackages: let
-        newKernel = kernelPackages.kernel.override {
-          kernelPatches = kernelPackages.kernel.kernelPatches
-                       ++ lib.singleton tunPatch;
-        };
-      in pkgs.linuxPackagesFor newKernel config.boot.kernelPackages;
-    };
-  };
+  services.journald.extraConfig = ''
+    MaxRetentionSec=3month
+  '';
 
-  config = {
-    networking.firewall.enable = false;
-
-    services.journald.extraConfig = ''
-      MaxRetentionSec=3month
-    '';
-
-    nixpkgs.config.packageOverrides = pkgs: {
-      inherit (import ./pkgs { inherit pkgs; }) headcounter;
-    };
+  nixpkgs.config.packageOverrides = pkgs: {
+    inherit (import ./pkgs { inherit pkgs; }) headcounter;
   };
 }
