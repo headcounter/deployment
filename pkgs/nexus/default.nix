2439889d9008a4b86914c6c6364b5b0e9768fc36
aszlig <aszlig@redmoonstudios.org>
2017-05-15 10:23:19 +0200

    nexus: Regenerate Nix expression file
    This is simply a re-run of cabal2nix, because with NixOS 17.03 we no
    longer need to include a newer version of the systemd library because
    version 1.1.2 is already in NixOS 17.03.
    
    Signed-off-by: aszlig <aszlig@redmoonstudios.org>

--- pkgs/nexus/default.nix
+++ pkgs/nexus/default.nix
@@ -1,29 +1,17 @@
-{ mkDerivation, async, attoparsec, base, bytestring, cereal, iproute, lens
-, network, stdenv, safecopy, stm, template-haskell, text, transformers, unix
+{ mkDerivation, async, attoparsec, base, bytestring, cereal
+, iproute, lens, network, process, safecopy, stdenv, stm, systemd
+, template-haskell, text, unix
 }:
-
-let
-  systemd = mkDerivation {
-    pname = "systemd";
-    version = "1.1.2";
-    sha256 = "11wjsfnnsfgrffsxy9s5yqlzb7zxlrjg92mhanq66jvbnqh1jijr";
-    libraryHaskellDepends = [ base bytestring network transformers unix ];
-    doCheck = false;
-    homepage = "https://github.com/erebe/systemd";
-    description = "Systemd facilities (Socket activation, Notify)";
-    license = stdenv.lib.licenses.bsd3;
-  };
-
-in mkDerivation {
+mkDerivation {
   pname = "nexus";
   version = "0.1.0.0";
   src = ./.;
   libraryHaskellDepends = [
-    attoparsec async base bytestring cereal iproute lens network
-    safecopy stm systemd template-haskell text
+    async attoparsec base bytestring cereal iproute lens network
+    process safecopy stm systemd template-haskell text
   ];
   testHaskellDepends = [
-    async base bytestring cereal network safecopy stm unix
+    async base bytestring cereal network stm unix
   ];
   license = stdenv.lib.licenses.agpl3;
 }
