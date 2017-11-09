f7f1b8683f2d44e7119670180d5f5a08eae54332
aszlig <aszlig@nix.build>
2017-11-10 00:55:23 +0100

    common.nix: Drop fix for ERL-340 on erlang
    This reverts commit 47badc0f8048410e655da5c0ddb1e422a7ed5c34.
    
    We no longer need this patch for NixOS 17.09 because it already contains
    the new upstream version with the fix.
    
    I've also switched to overlays instead of packageOverrides while at it.
    
    Signed-off-by: aszlig <aszlig@nix.build>

--- common.nix
+++ common.nix
@@ -1,3 +1,5 @@
+{ lib, ... }:
+
 {
   imports = import ./modules/module-list.nix;
   networking.firewall.enable = false;
@@ -11,19 +13,7 @@
     key.size = 4096;
   };
 
-  nixpkgs.config.packageOverrides = super: let
-    pkgs = super // self;
-    self = {
-      inherit (import ./pkgs { inherit pkgs; }) headcounter;
-
-      # https://bugs.erlang.org/browse/ERL-340
-      erlang = super.erlang.overrideDerivation (drv: {
-        patches = (drv.patches or []) ++ pkgs.lib.singleton (pkgs.fetchpatch {
-          url = "https://github.com/erlang/otp/commit/"
-              + "e27119948fc6ab28bea81019720bddaac5b655a7.patch";
-          sha256 = "1605hsdck7ng49yn9mjk7vsk12qr12ypjiwd7810ra9bva8x42m9";
-        });
-      });
-    };
-  in self;
+  nixpkgs.overlays = lib.singleton (self: lib.const {
+    inherit (import ./pkgs { pkgs = self; }) headcounter;
+  });
 }
